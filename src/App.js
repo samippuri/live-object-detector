5759ab16037e2ee4cec0fd41feda05d7cfde0696
samippuri <samippuri@vt.edu>
2024-08-16 15:07:29 -0400

    added webcam to react, imported necessary modules and packages
    Created a webcam for the react app and centered it to the screen, created a virtual environment to install all packages, created canvas variable to draw on live webcam.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import logo from './logo.svg';
+import React, {useRef,useState,useEffect} from 'react';
 import './App.css';
+import Webcam from 'react-webcam';
+import * as tf from '@tensorflow/tfjs';
+import * as cocossd from '@tensorflow-models/coco-ssd';
 
 function App() {
+  const webcamRef = useRef(null)
+  const canvasRef = useRef(null)
+
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className = "App-Header">
+        <Webcam 
+        ref={webcam}
+        muted={true}
+        style={{
+            position:'absolute',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+        }} 
+        />
+
+        <canvas>
+        ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 8,
+            width: 640,
+            height: 480,   
+          }}
+        </canvas>
       </header>
     </div>
   );
